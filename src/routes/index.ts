30f2788e20d9172a963b02d1a2b6943678896aba
flaviob4465 <flaviobrandao4465@gmail.com>
2025-07-22 16:54:55 +0100

    del

--- src/routes/index.ts
+++
@@ -1,7 +0,0 @@
-import express from "express"
-import { userRoutes } from "./users.route";
-
-export const routes = (app: express.Express) => {
-    app.use(express.json());
-    app.use(userRoutes);
-}
