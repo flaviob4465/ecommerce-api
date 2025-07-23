3585105d05680ac2b595bf8257c0b29a88292f06
flaviob4465 <flaviobrandao4465@gmail.com>
2025-07-23 01:01:24 +0100

    user iD

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,43 @@
-import express from 'express';
+import express, { Request, Response } from "express";
 
 const app = express();
 
-app.get('/', (req, res) =>{
-    res.send('Hello World!');
-} )
+app.use(express.json());
 
-app.listen(3000,()=>{
-    console.log('Servidor ativo na porta 3000');
-})
+// endpoint para retornar uma mensagem de boas-vindas
+// O método get recebe dois parâmetros, o primeiro é a rota e o segundo é uma função
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+let id = 0;
+// Middleware para permitir o envio de dados no corpo da requisição
+let usuarios: { id: number; nome: string; email: string }[] = [];
+
+
+// Endpoint para retornar uma lista de produtos
+app.get("/users", (req: Request, res: Response) => {
+  res.send(usuarios);
+});
+
+
+app.get("/users/:id", (req: Request, res: Response) =>{
+    let userId = Number (req.params.id);
+    let user = usuarios.find (user => user.id = userId) ;
+    res.send(user);
+});
+// Endpoint para adicionar um novo usuário
+app.post("/users", (req: Request, res: Response) => {
+let user = req.body;
+user.id = ++id;
+usuarios.push(user);
+    res.send({ 
+        message: "Usuário criado com sucesso!"
+    });
+});
+
+   
+
+app.listen(3000, () => {
+  console.log("Servidor ativo na porta 3000");
+});
